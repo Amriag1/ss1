8f9e204c4b416f48e36207e596f5101fd453b066
Amriag1 <amriag1@gmail.com>
2025-01-27 16:39:53 +0300

    Create ProgressBar.tsx

---
+++ `app/`app/`app/components/ProgressBar.tsx
@@ -0,0 +1,7 @@
+export default function ProgressBar() {
+  return (
+    <div className="fixed top-0 left-0 right-0 h-1 bg-gray-200 z-50">
+      <div className="h-full bg-gradient-to-r from-[#67C8F3] to-[#7DBB4F] w-1/3" />
+    </div>
+  )
+}
